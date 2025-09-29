bec1b973491d0a9713e5c529b79814cd1e4c341a
MekalaSindhu <mekalasaisindhu@gmail.com>
2025-09-29 11:50:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mini-dft-project-bucket"
+    bucket = "terraform-s3-remote-backend-statefile"
     key    = "Miniproject/terraform.tfstate"
     region = "us-west-1"
     encrypt = true
